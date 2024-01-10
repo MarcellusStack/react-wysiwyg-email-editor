46ebddf74ce5b307135fcfe752cd9dd79609547e
MarcellusStack <marcellusstack@gmail.com>
2024-01-10 20:40:08 +0100

    installed uuid, added preview back, add element

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Card,
@@ -8,17 +8,81 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ColorPicker } from "@/components/color-picker";
-import { GripHorizontal, Image } from "lucide-react";
+import { GripHorizontal, Image, FileDown, Plus } from "lucide-react";
+import { useEditorStore, type Element } from "@/lib/store";
+import { render } from "@react-email/render";
+import {
+  Html,
+  Tailwind,
+  Body,
+  Head,
+  Preview,
+  Container,
+  Section,
+  Text,
+} from "@react-email/components";
+import { v4 as uuidv4 } from "uuid";
 
 export const Editor = () => {
   const [enablePreview, setEnablePreview] = useState(false);
+  const elements = useEditorStore((state) => state.elements);
+  const selectedElement = useEditorStore((state) => state.selectedElement);
+  const selectElement = useEditorStore((state) => state.selectElement);
+  const addElement = useEditorStore((state) => state.addElement);
+  const renderElement = (element: Element) => {
+    const { id, element: elementType, classNames, children } = element;
+
+    // Customize this switch statement based on your element types
+    switch (elementType) {
+      case "section":
+        return (
+          <Section
+            key={id}
+            className={`${classNames} min-h-20 border hover:border-yellow-600`}
+          >
+            {children && children.map(renderElement)}
+          </Section>
+        );
+      case "text":
+        return (
+          <Text
+            key={id}
+            className={`${classNames} border hover:border-yellow-600`}
+            onClick={() => {
+              selectElement(element);
+            }}
+          >
+            {element.content}
+          </Text>
+        );
+
+      default:
+        return null; // Or handle unknown element types accordingly
+    }
+  };
+  const markup = render(
+    <Html>
+      <Head />
+      <Preview>Test</Preview>
+      <Tailwind>
+        <Body className="bg-white font-sans">
+          <Container className="w-full max-w-full">
+            {elements.map(renderElement)}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>,
+    { pretty: true },
+  );
 
   return (
     <div className="flex h-screen flex-col">
       {/* Header */}
-      <header className="sticky top-0 z-50 flex items-center justify-center p-4 text-white ">
+      <header className="sticky top-0 z-50 flex items-center justify-between  p-4 text-white ">
+        <div />
         <Tabs defaultValue="editor" className="w-[400px]">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger
@@ -39,6 +103,13 @@ export const Editor = () => {
             </TabsTrigger>
           </TabsList>
         </Tabs>
+        <Button
+          onClick={() => {
+            console.log(elements);
+          }}
+        >
+          <FileDown className="mr-2 h-4 w-4" /> Export
+        </Button>
       </header>
       <Separator />
       <div className="flex flex-1  overflow-y-auto">
@@ -60,7 +131,7 @@ export const Editor = () => {
                       <GripHorizontal className="h-4 w-4" />
                     </CardHeader>
                     <CardContent>
-                      <Image className="h-8 w-8" />
+                      <Image className="h-6 w-6" />
                     </CardContent>
                     <CardFooter>
                       <p className="text-sm font-medium leading-none">Image</p>
@@ -93,8 +164,33 @@ export const Editor = () => {
         <Separator orientation="vertical" />
         {/* Main Content */}
         <main className="flex-1 p-4">
-          {enablePreview && <div>Preview</div>}
-          {!enablePreview && <div>Editor</div>}
+          {enablePreview && (
+            <div
+              dangerouslySetInnerHTML={{ __html: markup }}
+              className="h-full w-full"
+            />
+          )}
+          {!enablePreview && (
+            <>
+              <div className="h-auto w-full">{elements.map(renderElement)}</div>
+              <Button
+                className="mt-2 w-full"
+                variant="secondary"
+                onClick={() => {
+                  addElement("", {
+                    id: uuidv4(),
+                    element: "section",
+                    isContainer: true,
+                    content: "",
+                    classNames: "",
+                    children: [],
+                  });
+                }}
+              >
+                <Plus className="mr-2 h-4 w-4" /> Add new section
+              </Button>
+            </>
+          )}
         </main>
         {/* Right Sidebar */}
         <Separator orientation="vertical" />
